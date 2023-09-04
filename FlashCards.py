f769406f0426fef1ec6570cba0ecfc5359502b17
Anton Drygin <73026946+AntonDrygin@users.noreply.github.com>
2023-09-04 21:57:51 +0300

    Update FlashCards.py

--- FlashCards.py
+++ FlashCards.py
@@ -5,7 +5,7 @@ import os
 
 translator = Translator()
 
-with open('D:\CATALOG\EDUCATION\PROGRAMMING\PHYTON\Scripts\Vocabulary\Vocabulary.txt', 'r', encoding='utf-8') as file:
+with open('Vocabulary.txt', 'r', encoding='utf-8') as file:
     file_lines = file.readlines()
 
 single_word_lines = [line.strip() for line in file_lines if len(line.split()) == 1]
@@ -15,10 +15,10 @@ random.shuffle(single_word_lines)
 batch_size = 10
 batches = [single_word_lines[i:i+batch_size] for i in range(0, len(single_word_lines), batch_size)]
 
-output_directory = 'D:\CATALOG\My Computer\My downloads\FlashCardsResalt'
+output_directory = 'FlashCardsResalt'
 os.makedirs(output_directory, exist_ok=True)
 
-timestamp = int(time.time())  # Текущее время в секундах с начала эпохи
+timestamp = int(time.time())  # Текущее время в секундах
 
 for batch_number, batch in enumerate(batches, start=1):
     session_batch_filename = f'fc_{timestamp}_{batch_number:02d}.txt'
