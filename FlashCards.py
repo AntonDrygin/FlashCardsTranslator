f9cb7199a578565efdefb8a09f511a61864b8927
Anton Drygin <73026946+AntonDrygin@users.noreply.github.com>
2023-09-04 21:48:59 +0300

    Add files via upload

---
+++ FlashCards.py
@@ -0,0 +1,35 @@
+import random
+import time
+from googletrans import Translator
+import os
+
+translator = Translator()
+
+with open('D:\CATALOG\EDUCATION\PROGRAMMING\PHYTON\Scripts\Vocabulary\Vocabulary.txt', 'r', encoding='utf-8') as file:
+    file_lines = file.readlines()
+
+single_word_lines = [line.strip() for line in file_lines if len(line.split()) == 1]
+
+random.shuffle(single_word_lines)
+
+batch_size = 10
+batches = [single_word_lines[i:i+batch_size] for i in range(0, len(single_word_lines), batch_size)]
+
+output_directory = 'D:\CATALOG\My Computer\My downloads\FlashCardsResalt'
+os.makedirs(output_directory, exist_ok=True)
+
+timestamp = int(time.time())  # Текущее время в секундах с начала эпохи
+
+for batch_number, batch in enumerate(batches, start=1):
+    session_batch_filename = f'fc_{timestamp}_{batch_number:02d}.txt'
+    full_filename = os.path.join(output_directory, session_batch_filename)
+    
+    print(f"Batch {batch_number}:\n")
+    
+    with open(full_filename, 'w', encoding='utf-8') as output_file:
+        for word in batch:
+            translation = translator.translate(word, src='en', dest='ru')
+            print(f"{word}={translation.text}")
+            output_file.write(f"{word}={translation.text}\n")
+    
+    print()  # Пустая строка между пакетами
